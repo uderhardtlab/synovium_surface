e40897adcd469ee5a7994d9261b9910cd10a4d8c
Anna Moeller <je30bery@idea-weneg.aibe.uni-erlangen.de>
2024-11-12 13:19:12 +0100

    morph grad approach

--- src/surface_extraction.py
+++ src/surface_extraction.py
@@ -1,31 +1,91 @@
-import json
 import matplotlib.pyplot as plt
 import numpy as np
+from cupyx.scipy.ndimage import median_filter, gaussian_filter, label, morphological_gradient
+from skimage.filters import sobel
+from skimage.segmentation import expand_labels
+from skimage import img_as_float
+from scipy.ndimage import binary_closing
 from scipy.interpolate import griddata
-
-from skimage.measure import regionprops, label
+import json
 import tifffile as tf
-from tqdm import tqdm
+import seaborn as sns
+import sys
+import cupy as cp
 
-cuda_device = True
-import scipy as sc
+def smooth_image_in_xyz(image, size):
+    image = cp.array(image)
+    smoothed = median_filter(image.astype(int), size=(size, size, size))
+    del image
+    return smoothed
 
-if cuda_device:
-    from cupyx.scipy.ndimage import median_filter
-    from cupy import diff, argmax, where, nan_to_num, indices, zeros, clip, nanmedian, isnan
-    import cupy as cp
+
+def get_biggest_connected_component(binary_image):
+    labelled, num_labels = label(cp.array(binary_image.astype(bool)), structure=cp.ones((3, 3, 3)))
+    del binary_image
+    v, c = cp.unique(labelled, return_counts=True)
+    v_max = v[1:][cp.argmax(c[1:])]
+    biggest_connected_volume = labelled == v_max
+    del labelled
+    return biggest_connected_volume
+
+
+def get_morph_gradient(biggest_connected_volume):
+    kernel = cp.zeros((3, 3, 3))
+    kernel[1,1,:] = 1
+    kernel[:,1,1] = 1
+    kernel[1,:,1] = 1
+    morph_grad = morphological_gradient(biggest_connected_volume.astype(int), structure=kernel)
+    return morph_grad
+
+
+def get_layer_in_3d(top_layer, image_shape):
+    if isinstance(top_layer, np.ndarray):
+        top_layer = cp.array(top_layer)
+    top_layer = cp.nan_to_num(top_layer, 0)
+    x, y = cp.indices(top_layer.shape) 
+    top_layer_3d = cp.zeros(image_shape)
+    top_layer = cp.clip(top_layer, 0, image_shape[0] - 1).astype(int)
+    top_layer_3d[top_layer, x, y] = 1
+    top_layer_3d[0, :, :] = 0
+    return cp.asarray(top_layer_3d)
+
+
+def get_top_layer(morph_grad, img_shape, bottom=False):
+    if not bottom:
+        top_layer = cp.argmax(morph_grad, axis=0)
+        top_layer_3d = get_layer_in_3d(top_layer, img_shape)
+        del top_layer
+        return top_layer_3d
+    else:
+        bottom_layer = cp.argmax(morph_grad[::-1], axis=0)
+        bottom_layer_3d = get_layer_in_3d(bottom_layer, img_shape)[::-1]
+        del bottom_layer
+        return bottom_layer_3d
+
+
+def expand_labels_on_morph(morph_grad, img_shape):
+    labels = get_top_layer(morph_grad, img_shape, bottom=False) + 2 * get_top_layer(morph_grad, img_shape, bottom=True)
+    expanded_labels = expand_labels(cp.asnumpy(labels), distance=20, spacing=(10, 1, 1)) # this corresponds to expanding by 2 in z dimension and by 20 in x and y dimensions, so it should definetely cover all pertrusions
+    expanded_labels_on_morph = cp.array(expanded_labels == 1).astype(int) * (morph_grad == np.max(morph_grad))
+    return expanded_labels_on_morph
+
+
+def process_patch(image, binarization_threshold=6):
+    smoothed_image = smooth_image_in_xyz(image, size=5)
+    binary_image = smoothed_image > binarization_threshold
+    del smoothed_image
+
+    biggest_connected_volume = get_biggest_connected_component(binary_image)
+    shape_3d = biggest_connected_volume.shape
+    morph_grad = get_morph_gradient(biggest_connected_volume)
+    surface = expand_labels_on_morph(morph_grad, shape_3d)
+    del morph_grad
+    del biggest_connected_volume
+    return cp.asnumpy(surface)
     
-    
-else:
-    from scipy.ndimage import median_filter
-    from numpy import diff, argmax, where, nan_to_num, indices, zeros, clip, nanmedian, isnan
 
-def smooth_image_in_xyz(image):
-    if cuda_device:
-        if isinstance(image, np.ndarray):
-            image = cp.array(image)
-    return median_filter(image.astype(int), size=(3, 3, 3))
-
+"""
+old
 
 def get_top_layer(binary_image):
     gradient_z = diff(binary_image, axis=0)  # estimates gradient along z
@@ -35,17 +95,6 @@ def get_top_layer(binary_image):
     top_layer[where(top_layer) == 0] = np.nan
     return top_layer
 
-
-def get_layer_in_3d(top_layer, image_shape):
-    if isinstance(top_layer, np.ndarray):
-        top_layer = cp.array(top_layer)
-    top_layer = nan_to_num(top_layer, 0)
-    x, y = indices(top_layer.shape) 
-    top_layer_3d = zeros(image_shape)
-    top_layer = clip(top_layer, 0, image_shape[0] - 1).astype(int)
-    top_layer_3d[top_layer, x, y] = 1
-    top_layer_3d[0, :, :] = 0
-    return cp.asarray(top_layer_3d)
 
 
 def patch(top_layer, median_filtered_interp_top_layer):
@@ -76,3 +125,4 @@ def median_filtering_in_xy(top_layer, size):
     if isinstance(top_layer, np.ndarray):
         top_layer = cp.array(top_layer)
     return median_filter(top_layer, size=size)
+"""
